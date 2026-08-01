673323c2245754659526b8e969f3c4d852507d8b
RODRIGUEZ LOPEZ Antonio <rodriguezla@unicc.org>
2026-08-01 21:41:27 +0200

    launch chrome

--- src/mymcp/crawl4ai/server.py
+++ src/mymcp/crawl4ai/server.py
@@ -15,7 +15,33 @@ def ensure_playwright_browsers():
             check=True,
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
-        )        
+        )      
+        
+ import subprocess
+import threading
+import time
+
+def start_chrome_headless():
+    def _run():
+        process = subprocess.Popen(
+            [
+                "/opt/playwright-browsers/chromium-1234/chrome-linux64/chrome",
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--remote-debugging-port=9222",
+                "--remote-debugging-address=0.0.0.0",
+                "about:blank"
+            ],
+            stdout=open("/tmp/chrome.log", "w"),
+            stderr=subprocess.DEVNULL
+        )
+        start_chrome_headless.process = process
+        time.sleep(4)
+
+    thread = threading.Thread(target=_run, daemon=True)
+    thread.start()
+    return thread         
 
 from ..server import MCPServer
 from . import Crawler
@@ -25,6 +51,7 @@ crawler = Crawler()
 def run():
 
     ensure_playwright_browsers()
+    start_chrome_headless()
     
     crawler = Crawler()
     
